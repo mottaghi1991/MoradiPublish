fa5b9535a41cdde15314e80d176d5839bc5e121c
Ali Mottaghi <ali.mottaghi1991@gmail.com>
2025-10-08 17:54:59 +0330

    add location

---
+++ wwwroot/js/FillSelectList.js
@@ -0,0 +1,23 @@
+﻿function GetCityByProvinceId(Id) {
+    $.ajax({
+        url: "../../Shop/GetCityByProvinceId?ProId=" + Id,
+        method: "GET",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            var s = '<option value="-1">شهرها</option>';
+            for (var i = 0; i < data.length; i++) {
+                console.log(data[i].value);
+                console.log(data[i].text);
+
+                s += '<option value="' + data[i].value + '">' + data[i].text + '</option>';
+            }
+            $("#City").html(s);
+        },
+        error: function (data) {
+            console.log(data);
+
+        }
+    });
+};
